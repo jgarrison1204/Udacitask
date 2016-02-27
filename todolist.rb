813cd7b195f9561107eac9ed19d2a52f27bb0753
Jacob Garrison <jgarrison1204@gmail.com>
2016-02-26 17:24:18 -0800

    Adding and deleting todos functional

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,49 @@
 class TodoList
-    # methods and stuff go here
+  attr_accessor :title, :items
+
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new # Starts empty! No Items yet!
+  end
+
+  def rename_title(new_title)
+    @title = new_title
+  end
+
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items << item
+  end
+
+  def delete_item(index)
+    @items.delete_at(index)
+  end
 end
 
 class Item
-    # methods and stuff go here
+  attr_accessor :description, :completed_status
+
+  def initialize(item_description)
+    @description = item_description
+    @completed_status = false
+  end
+
+  def print_stuff
+    puts @description
+  end
+
+
+  def due_date
+
+  end
+
+  def completed?
+    if @completed_status == true
+      #do something
+    else
+      #do somethine else
+    end
+  end
+
+
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,40 @@
 require_relative 'todolist.rb'
 
+
 # Creates a new todo list
+my_list = TodoList.new("Shit To Do:")
 
 # Add four new items
+my_list.add_item("Go to the gym")
+my_list.add_item("Draw a plan to save the Lakers")
+my_list.add_item("Drink heavily after realizing there is no hope for the Lakers")
+my_list.add_item("Take out the trash")
 
 # Print the list
+puts my_list.title
+my_list.print_items
+puts
 
 # Delete the first item
+my_list.delete_item(0)
 
 # Print the list
+puts
+my_list.print_items
 
 # Delete the second item
+my_list.delete_item(0)
 
 # Print the list
+puts
+my_list.print_items
 
 # Update the completion status of the first item to complete
 
 # Print the list
 
 # Update the title of the list
+rename_title("Fuck This!")
+
 
 # Print the list
